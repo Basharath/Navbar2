add8d29d15c70f994867a81446fb914048871e86
Basharath <basharath.in@gmail.com>
2021-01-07 22:25:13 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Box from "./components/Box";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <Box />
+    </>
+  );
+}
